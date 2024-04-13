e8d96b79fd4b38881cfb4c2da36b95645501e298
Krishnaprasad K <krishnanova@gmail.com>
2024-04-13 07:43:45 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const dotenv = require("dotenv").config({ path: __dirname + "/client/.env" });
+console.log(__dirname + "/client/.env");
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  entry: "./client/src/index.tsx", // Entry point of your React application
+  output: {
+    path: path.resolve(__dirname, "dist"), // Output directory
+    filename: "bundle.js", // Output bundle filename
+    publicPath: "/", // Public path for assets (e.g., '/static/')
+  },
+  module: {
+      rules: [
+          // JavaScript/JSX files
+          {
+              test: /\.(js|jsx)$/,
+              // test: /\.js$|jsx/,
+              exclude: /node_modules/,
+              use: {
+                  loader: 'babel-loader', // Use Babel to transpile JavaScript
+                  options: {
+                      presets: ['@babel/preset-env', '@babel/preset-react'], // Babel presets
+                  },
+              },
+          },
+          // Typescript/TSX files
+          {
+              test: /\.(ts|tsx)$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: 'ts-loader', // Use Babel to transpile JavaScript
+              },
+          },
+          // CSS files (optional)
+          {
+              test: /\.css$/,
+              use: ['style-loader', 'css-loader'], // Use style-loader and css-loader
+          },
+      ],
+  },
+ 
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(dotenv.parsed),
+      "process.env.NODE_ENV": JSON.stringify(
+        isDevelopment ? "development" : "production"
+      ),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./client/src/index.html", // Path to HTML template
+      // favicon: './public/favicon.ico', // Ppath to favicon (optional)
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true, // Enable HTML5 History API fallback
+  },
+};
